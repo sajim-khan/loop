1f9e30a2b6f680a721b724fd92a87316201ed812
sajim-khan <shossain1427@gmail.com>
2023-01-29 15:53:33 +0600

    added

--- first.js
+++ first.js
@@ -6,3 +6,12 @@ if (a < b) {
 } else {
     console.log("a is greater than b")
 }
+// a = 5;
+// b = 6;
+
+// if (a < b) {
+//     console.log("b is greater than a")
+// } else {
+//     console.log("a is greater than b")
+// }
+
