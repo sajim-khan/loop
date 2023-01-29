5570e29a773bd94dd2296139ecb2af1751ac8a6c
sajim-khan <shossain1427@gmail.com>
2023-01-29 22:47:51 +0600

    added

---
+++ while-loop.js
@@ -0,0 +1,18 @@
+// while loop
+
+/**
+ * 1. Loop variable
+ * 2. condition inside
+ * 3. loop body
+ * 4. change the loop variable (increment or decrement)
+ */
+
+var chickenRoast = 0;
+while(chickenRoast < 7){
+    console.log('Roast den please!!')
+    console.log(chickenRoast);
+    chickenRoast += 1;
+    //console.log(chickenRoast);
+}
+
+
