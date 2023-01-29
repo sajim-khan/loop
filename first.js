550cd8203465677e20a0c4d2c6590cdc714ac104
sajim-khan <shossain1427@gmail.com>
2023-01-29 21:58:42 +0600

    added

--- first.js
+++ first.js
@@ -6,12 +6,16 @@ if (a < b) {
 } else {
     console.log("a is greater than b")
 }
-// a = 5;
-// b = 6;
 
-// if (a < b) {
-//     console.log("b is greater than a")
-// } else {
-//     console.log("a is greater than b")
-// }
 
+var tableItems = ['pen', 'pencil', 'paper', 'book', 'mouse', 'bottle'];
+//tableItems.pop();
+tableItems.shift();
+tableItems.push('laptop');
+//tableItems.unshift('table');
+console.log(tableItems);
+var item3 = tableItems[3];
+console.log(item3);
+tableItems[6] = 'notebook';
+console.log(tableItems);
+tableItems.pop();
