2d10a2dd07fb65cdc0e7cdcd5e466a77a2b2e063
sajim-khan <shossain1427@gmail.com>
2023-01-30 23:30:56 +0600

    added

--- array-loop.js
+++ array-loop.js
@@ -1,6 +1,13 @@
+// var numbers = [12, 23,45, 67, 78, 89, 90, 43, 65, 76, 32];
+// for (var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
 
 var numbers = [12, 23,45, 67, 78, 89, 90, 43, 65, 76, 32];
 for (var i = 0; i < numbers.length; i++){
     var number = numbers[i];
     console.log(number);
 }
+
+
