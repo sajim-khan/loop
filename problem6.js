d68aad134ac54454ef7f5156b9ed905ff51f30ed
sajim-khan <shossain1427@gmail.com>
2023-02-05 04:06:02 +0600

    added some solution

---
+++ problem6.js
@@ -0,0 +1,24 @@
+var bangla = 75.25;
+var math = 65;
+var biology = 80;
+var chemistry = 35.45;
+var physics = 99.50;
+var totalMarks = bangla + math + physics + chemistry + biology;
+console.log(totalMarks);
+var average = totalMarks / 5;
+var avg = average.toFixed(2);
+console.log(avg);
+
+
+//another example
+
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer';
+var sentence = firstLine + ' ' + secondLine;
+console.log(sentence);
+
+
+//another example
+var givenNumber = 119;
+var reminder = givenNumber % 5;
+console.log('reminder is :',reminder);
