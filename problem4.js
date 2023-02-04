a9ff359f2077ad53ced5472e35489852a8346675
sajim-khan <shossain1427@gmail.com>
2023-02-05 01:41:01 +0600

    added isosceles solution

---
+++ problem4.js
@@ -0,0 +1,13 @@
+var side1 = 4;
+var side2 = 5;
+var side3 = 3;
+
+if (side1 == side2 || side1 == side3 || side2 == side3){
+    console.log(" The triangle is isosceles");
+}
+// } else if (side1 === side2 === side3 ){
+//     console.log(" The triangle is equilateral");
+// } 
+else {
+    console.log(" This is only triangle");
+}
