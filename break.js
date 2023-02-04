9cc3b021abb6fe90d480e72a0b40abf0a992fe96
sajim-khan <shossain1427@gmail.com>
2023-02-04 23:51:27 +0600

    added

--- break.js
+++ break.js
@@ -15,7 +15,15 @@
 // }
 
 
-for (var i = 1; i < 15; i++){
+// for (var i = 1; i < 15; i++){
+//     console.log(i);
+//    if (i > 10){
+//     break;
+//    }
+   
+// }
+
+for (var i = 1; i < 20; i++){
     console.log(i);
    if (i > 10){
     break;
