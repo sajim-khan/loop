65604a337dd5013cb0fb24632d14f27fc389192c
sajim-khan <shossain1427@gmail.com>
2023-02-05 03:35:55 +0600

    added reverse way in loop

---
+++ reverse.js
@@ -0,0 +1,20 @@
+var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num ++;
+}
+
+//while loop reverse way
+console.log('reverse way in while loop:');
+var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num --;
+}
+
+
+//for loop reverse way
+console.log('reverse way in for loop:');
+for (var i = 10; i >= 1; i--){
+    console.log(i);
+}
