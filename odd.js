ddcce9de4751018e94c70ad05b3b9fe94f1d8956
sajim-khan <shossain1427@gmail.com>
2023-02-04 23:54:46 +0600

    added

--- odd.js
+++ odd.js
@@ -12,7 +12,13 @@ for (var i = 1; i < 10; i += 2){
 }
 
 // odd in for loop
-for (var i = 1; i < 10; i += 2){
+// for (var i = 1; i < 10; i += 2){
+//     console.log(i);
+//     console.log('Roast den please!!');
+// }
+
+
+for (var i = 1; i < 10; i += 5){
     console.log(i);
     console.log('Roast den please!!');
 }
