f9d0963c91f7f8f7e901f6320fdb5c8f5a33d8f7
sajim-khan <shossain1427@gmail.com>
2023-02-05 01:03:13 +0600

    added grade syatem solution

--- problem2.js
+++ problem2.js
@@ -1,1 +1,69 @@
-console.log("problem 2");
+// My grade
+var myScore = 85;
+if (myScore >= 80){
+    console.log("My grade is A");
+} else if (myScore >= 60){
+    console.log("My grade is B");
+} else if (myScore >= 50){
+    console.log("My grade is C");
+} else if (myScore >= 40){
+    console.log("My grade is D");
+} else if (myScore <= 39){
+    console.log("My grade is F");
+}
+
+//Tom grade
+var tomScore = 66;
+if (tomScore >= 80){
+    console.log("Tom's grade is A");
+} else if (tomScore >= 60){
+    console.log("Tom's grade is B");
+} else if (tomScore >= 50){
+    console.log("Tom's grade is C");
+} else if (tomScore >= 40){
+    console.log("Tom's grade is D");
+} else if(tomScore <= 39){
+    console.log("Tom's grade is F");
+}
+
+// Jane grade
+var janeScore = 95;
+if (janeScore >= 80){
+    console.log("Jane's grade is A");
+} else if (janeScore >= 60){
+    console.log("Jane's grade is B");
+} else if (janeScore >= 50){
+    console.log("Jane's grade is C");
+} else if (janeScore >= 40){
+    console.log("Jane's grade is D");
+} else if(janeScore <= 39){
+    console.log("Jane's grade is F");
+}
+
+// Peter grade
+var peterScore = 56;
+if (peterScore >= 80){
+    console.log("Peter's grade is A");
+} else if (peterScore >= 60){
+    console.log("Peter's grade is B");
+} else if (peterScore >= 50){
+    console.log("Peter's grade is C");
+} else if (peterScore >= 40){
+    console.log("Peter's grade is D");
+} else if(peterScore <= 39){
+    console.log("Peter's grade is F");
+}
+
+//John grade
+var johnScore = 40;
+if (johnScore >= 80){
+    console.log("John's grade is A");
+} else if (johnScore >= 60){
+    console.log("John's grade is B");
+} else if (johnScore >= 50){
+    console.log("John's grade is C");
+} else if (johnScore >= 40){
+    console.log("John's grade is D");
+} else if(johnScore <= 39){
+    console.log("John's grade is F");
+}
