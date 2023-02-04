2eeccf8d22913c354b8e9acde1e5643cbe72109e
sajim-khan <shossain1427@gmail.com>
2023-02-05 03:48:23 +0600

    added pricing problem solution

---
+++ problem5.js
@@ -0,0 +1,8 @@
+var momGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+console.log('Mom Given : 1000')
+var totalPrice = applePrice + orangePrice;
+console.log('TotalPrice: ',totalPrice);
+var getBack = momGiven - totalPrice;
+console.log('GetBack: ',getBack);
